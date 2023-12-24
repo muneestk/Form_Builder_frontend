602b454154d17f7527371371e714c2b4c5d7f4fb
muneestk <125794679+muneestk@users.noreply.github.com>
2023-12-24 22:29:03 +0530

    project updated 2

--- src/environment/environment.develepment.ts
+++ src/environment/environment.develepment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
     production:false,
-    User_API_Key : "http://localhost:3000",
-    Local_API :  "https://formbuilderbackend.onrender.com"
+    user_API_Key : "http://localhost:3000",
+    User_API_Key :  "https://formbuilderbackend.onrender.com"
 };
